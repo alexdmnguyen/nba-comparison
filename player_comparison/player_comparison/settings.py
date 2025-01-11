121d1f251243906ae1dc5212074cc5529e325a36
alexdmnguyen <alexdmnguyen@gmail.com>
2025-01-10 18:10:39 -0800

    changing settings and wsgi files

--- player_comparison/player_comparison/settings.py
+++ player_comparison/player_comparison/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-gsi^ywq8a=6f3vhgzr7*l1*@$j-t*q=pqr&$-w+5nhr2ahj($9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://nba-comparison.onrender.com']
 
 
 # Application definition
