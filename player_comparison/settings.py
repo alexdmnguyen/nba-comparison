89461ce1114d0a2422ebe337b493078eac2a4ad6
alexdmnguyen <alexdmnguyen@gmail.com>
2025-01-10 20:03:05 -0800

    static files

--- player_comparison/settings.py
+++ player_comparison/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-gsi^ywq8a=6f3vhgzr7*l1*@$j-t*q=pqr&$-w+5nhr2ahj($9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'nba-comparison.onrender.com']
+ALLOWED_HOSTS = ['nba-comparison.onrender.com']
 
 
 # Application definition
@@ -116,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
